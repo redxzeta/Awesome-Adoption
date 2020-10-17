f1cb5101de93563e9ac8ad57767edcea88ff15e1
redxzeta <nsuarez.22@gmail.com>
2020-10-17 13:05:53 -0700

    adding layout and navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavigationBar from "./components/layout/NavigationBar";
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/pets">
-          <h2>pets</h2>
-        </Route>
-        <Route path="/">
-          <h1>HOME</h1>
-        </Route>
-      </Switch>
-    </Router>
+    <Fragment>
+      <NavigationBar />
+      <Container>
+        <Router>
+          <Switch>
+            <Route path="/pets">
+              <h2>pets</h2>
+            </Route>
+            <Route path="/">
+              <h1>HOME</h1>
+            </Route>
+          </Switch>
+        </Router>
+      </Container>
+    </Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
