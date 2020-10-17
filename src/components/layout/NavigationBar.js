dde8923a1a7f3bcbbd6b6246d472c73c9ec1be09
redxzeta <nsuarez.22@gmail.com>
2020-10-17 13:18:41 -0700

    adding themes

--- src/components/layout/NavigationBar.js
+++ src/components/layout/NavigationBar.js
@@ -3,7 +3,7 @@ import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export default function NavigationBar() {
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="primary" expand="lg">
       <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
