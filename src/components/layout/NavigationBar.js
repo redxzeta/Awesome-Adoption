0e4f0005750d5f3c691cc4721c4819557bbbd5a5
redxzeta <xzetanzero@gmail.com>
2020-10-21 09:12:44 -0700

    removing the extra dropdown

--- src/components/layout/NavigationBar.js
+++ src/components/layout/NavigationBar.js
@@ -51,17 +51,6 @@ export default function NavigationBar({ token }) {
               All Pets
             </NavDropdown.Item>
           </NavDropdown>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
