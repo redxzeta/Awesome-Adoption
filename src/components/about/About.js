791a5e631d64a24fb4d1718ea4597356c5070e1e
masayaShinoda <masayashida@gmail.com>
2020-11-29 15:12:08 +0700

    About page contributors list now render from github api, fixed mobile overflow

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,23 +1,38 @@
 import React from "react";
+import { useState, useEffect } from "react";
+
 import { Image } from "react-bootstrap";
 import Dog from "./dog.jpg";
 import GitHubLogo from "./GitHub-Mark-Light-120px-plus.png";
 import "./about.css";
 export default function About() {
-  const contributorAvatars = [
-    "https://avatars1.githubusercontent.com/u/19883954?s=150&v=4",
-     "https://avatars1.githubusercontent.com/u/17388273?s=150&v=4",
-     "https://avatars3.githubusercontent.com/u/34899426?s=150&v=4",
-     "https://avatars0.githubusercontent.com/u/1711057?s=150&v=4",
-     "https://avatars2.githubusercontent.com/u/61450115?s=150&v=4",
-     "https://avatars2.githubusercontent.com/u/54863160?s=150&v=4",
-     "https://avatars2.githubusercontent.com/u/54453166?s=150&v=4",
-     "https://avatars3.githubusercontent.com/u/13179054?s=150&v=4",
-     "https://avatars3.githubusercontent.com/u/55332102?s=150&v=4",
-     "https://avatars2.githubusercontent.com/u/41325679?s=150&v=4",
-     "https://avatars1.githubusercontent.com/u/46096911?s=460&u=38b7654de9a335f3a2bc35275634cdb548994fbc&v=4"
-    ];
+  // const data = [];
+  // const contributorsAvatars = [];
+  // const contributorsIds = [];
+  // let i = 0;
+   
+  // axios.get('https://api.github.com/repos/redxzeta/Awesome-Adoption/contributors').then(resp => {
 
+  //   // console.log(resp.data);
+  //   data.push(resp.data);
+  //   // console.log(contributorAvatars[0].length)
+  //   for (i=0; i < data[0].length; i++) {
+  //     contributorsAvatars.push(data[0][i]["avatar_url"])
+  //     contributorsIds.push(data[0][i]["id"])
+  //   }
+  // }).then(console.log(contributorsAvatars))
+  const axios = require('axios');
+  const [avatars, setAvatars] = useState((avatars) => {return null}); // by default there are no avatars
+  const apiUrl = "https://api.github.com/repos/redxzeta/Awesome-Adoption/contributors";
+  const fetchData = async () => {
+    const response = await axios.get(apiUrl)
+    setAvatars(response.data)
+    console.log(response.data)
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
   return (
     <div className="about__container">
       <Image src={Dog} alt="doggo" roundedCircle />
@@ -29,13 +44,15 @@ export default function About() {
       </p>
       <div className="contributors-section">
         <h1>Contributors</h1>
-        <div className="contributors">
-          {contributorAvatars.map(a => {
-              return <img src={a} alt="Contributor Avatar"></img>
-          })}
-
+        <div className="contributors" id="contributors">  
+          {avatars &&
+            avatars.map(a => {
+              return <img key={a.id} src={a.avatar_url} alt="Contributor Avatar"/ >
+            })
+          }
         </div>
-        <a href="https://github.com/redxzeta/Awesome-Adoption" target="_blank"><img src={GitHubLogo} alt="GitHub logo"></img><p>GitHub</p></a>
+        <a href="https://github.com/redxzeta/Awesome-Adoption" target="_blank" rel="noreferrer"><img src={GitHubLogo} alt="GitHub logo"></img><p>GitHub</p></a>
+        
       </div>
     </div>
   );
