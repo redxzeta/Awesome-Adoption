ec5d464bbe0c7e1c75999bc45645e2dbf4e7b0db
redxzeta <xzetanzero@gmail.com>
2020-12-06 14:11:19 -0800

    adding in some testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  test("renders App component", () => {
+    render(<App />);
+  });
 });
