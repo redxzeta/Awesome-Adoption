5f625534d679507e9b823f147530c4f94f2e6635
redxzeta <xzetanzero@gmail.com>
2021-10-10 19:49:19 -0700

    supabase registration and useForm hook

---
+++ src/components/accounts/Register.jsx
@@ -0,0 +1,50 @@
+import { Form, Button, Container } from "react-bootstrap";
+import useForm from "../../useHooks/useForm";
+
+const initState = {
+  email: "",
+  password: "",
+};
+
+const Register = () => {
+  const userRegister = () => console.log(form);
+  const [form, handleChange, onSubmit] = useForm(initState, userRegister);
+  return (
+    <Container flud="md">
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            onChange={handleChange}
+            value={form.email}
+          />
+          <Form.Text className="text-muted">
+            We will never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+            value={form.password}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Register;
