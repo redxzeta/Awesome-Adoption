87c539d79bdf17a4391e7eb19822784e516056e3
redxzeta <xzetanzero@gmail.com>
2021-10-10 19:49:19 -0700

    supabase registration and useForm hook

--- src/utils/SupaBaseUtils.js
+++ src/utils/SupaBaseUtils.js
@@ -4,10 +4,7 @@ const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_URL_KEY_PUB;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-<<<<<<< HEAD
 
 export const signUp = (form) => supabase.auth.signUp(form);
 
 export const signIn = (login) => supabase.auth.signIn(login);
-=======
->>>>>>> 50af7c7 (supautils)
