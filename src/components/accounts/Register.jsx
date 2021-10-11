fbea9bf995157b340218186b9a811a0702335d37
redxzeta <xzetanzero@gmail.com>
2021-10-11 06:54:19 -0700

    removing check

--- src/components/accounts/Register.jsx
+++ src/components/accounts/Register.jsx
@@ -36,9 +36,6 @@ const Register = () => {
             value={form.password}
           />
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
         <Button variant="primary" type="submit">
           Submit
         </Button>
