75291fb777f9c7509b8c4c6a84fbeab8ec4e8f60
NicolasBiondini <nicolasbiondiniwork@gmail.com>
2021-10-11 16:48:23 -0300

    sing up page done

--- src/components/accounts/Register.jsx
+++ src/components/accounts/Register.jsx
@@ -1,5 +1,6 @@
 import { Form, Button, Container } from "react-bootstrap";
 import useForm from "../../useHooks/useForm";
+import "./register.css";
 
 const initState = {
   email: "",
@@ -10,36 +11,39 @@ const Register = () => {
   const userRegister = () => console.log(form);
   const [form, handleChange, onSubmit] = useForm(initState, userRegister);
   return (
-    <Container flud="md">
-      <Form onSubmit={onSubmit}>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            name="email"
-            onChange={handleChange}
-            value={form.email}
-          />
-          <Form.Text className="text-muted">
-            We will never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <Container id="container" flud="md">
+      <div id="form-container">
+        <h1 id="form-title">Sing Up</h1>
+        <Form id="form" onSubmit={onSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              onChange={handleChange}
+              value={form.email}
+            />
+            <Form.Text className="text-muted">
+              We will never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={handleChange}
-            value={form.password}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              value={form.password}
+            />
+          </Form.Group>
+          <Button id="submit-button" variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </Container>
   );
 };
