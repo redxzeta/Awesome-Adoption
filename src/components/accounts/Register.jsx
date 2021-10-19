8a59a2263e0d756783e6514328a46e159818cdeb
redxzeta <xzetanzero@gmail.com>
2021-10-18 20:48:40 -0700

    merging

--- src/components/accounts/Register.jsx
+++ src/components/accounts/Register.jsx
@@ -1,6 +1,5 @@
 import { Form, Button, Container } from "react-bootstrap";
 import useForm from "../../useHooks/useForm";
-import "./register.css";
 
 const initState = {
   email: "",
@@ -11,39 +10,39 @@ const Register = () => {
   const userRegister = () => console.log(form);
   const [form, handleChange, onSubmit] = useForm(initState, userRegister);
   return (
-    <Container className="register__container" flud="md">
-      <div className="register__container_form">
-        <h1 className="register__title">Sign Up</h1>
-        <Form className="register__form" onSubmit={onSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              name="email"
-              onChange={handleChange}
-              value={form.email}
-            />
-            <Form.Text className="text-muted">
-              We will never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+    <Container flud="md">
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            onChange={handleChange}
+            value={form.email}
+          />
+          <Form.Text className="text-muted">
+            We will never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              name="password"
-              onChange={handleChange}
-              value={form.password}
-            />
-          </Form.Group>
-          <Button className="register__button" variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </div>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+            value={form.password}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </Container>
   );
 };
