50c7ccf1a22a847eae586068c1980514f6a94351
Vanessa Cassandra <vanessa.cassandra@juliusbaer.com>
2021-10-25 21:47:11 +0800

    Add icon attribution in the footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -10,6 +10,18 @@ const Footer = () => {
       <Row>
         <span>&copy; redxzeta 2020 - {year.getFullYear()}</span>
       </Row>
+      <Row>
+        <span>
+          Icons made by{" "}
+          <a href="https://www.freepik.com" title="Freepik">
+            Freepik
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </span>
+      </Row>
     </Container>
   );
 };
