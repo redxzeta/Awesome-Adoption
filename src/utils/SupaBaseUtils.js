44e093a5bc89a03c6ec4afda6a328b55c03ec0b9
redxzeta <xzetanzero@gmail.com>
2021-10-25 20:49:54 -0700

    fixing utils

--- src/utils/SupaBaseUtils.js
+++ src/utils/SupaBaseUtils.js
@@ -4,7 +4,3 @@ const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_URL_KEY_PUB;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-export const signUp = (form) => supabase.auth.signUp(form);
-
-export const signIn = (login) => supabase.auth.signIn(login);
