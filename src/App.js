229622f51572e8d7e8bca6b2dd8f375e8c279e9a
redxzeta <xzetanzero@gmail.com>
2021-10-25 21:05:46 -0700

    app+ register

--- src/App.js
+++ src/App.js
@@ -65,16 +65,6 @@ export default function App() {
     setAuthenticated(true);
   }, []);
 
-  const [session, setSession] = useState(null);
-
-  useEffect(() => {
-    setSession(supabase.auth.session());
-
-    supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-  }, []);
-  console.log(session);
   return (
     <Fragment>
       <TokenContext.Provider value={token}>
