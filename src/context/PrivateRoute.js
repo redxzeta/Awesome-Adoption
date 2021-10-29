9203e7ab9832a1efbab88d65e5b6bc50d3faf64f
drew5494 <tassoone@gmail.com>
2021-10-28 21:28:44 -0400

    Add private route

---
+++ src/context/PrivateRoute.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isLogin } from "../utils";
+import { useAuth } from "./SupaContext";
+const { session } = useAuth();
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        session ? <Component {...props} /> : <Redirect to="/" />
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
