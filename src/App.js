9858ef0a0af089bc656566103f7b66a1e4fcd042
drew5494 <tassoone@gmail.com>
2021-10-28 21:31:38 -0400

    Add private route for reset password

--- src/App.js
+++ src/App.js
@@ -99,11 +99,9 @@ export default function App() {
                     path="/forgot-password"
                     component={ForgotPassword}
                   />
-                  <Route
-                    exact
-                    path="/reset-password"
-                    component={ResetPassword}
-                  />
+                  <Route path="/reset-password" exact>
+                    {Authenticated && <ResetPassword />}
+                  </Route>
                   <Route path="/" exact>
                     {Authenticated && <Home />}
                   </Route>
