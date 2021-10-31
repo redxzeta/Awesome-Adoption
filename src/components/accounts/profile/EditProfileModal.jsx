ff0e37eba6544d2ab28e71e0f158a31459dad918
dhillonks <kunwarvir@hotmail.com>
2021-10-31 01:31:44 -0400

    adding validation to the form

--- src/components/accounts/profile/EditProfileModal.jsx
+++ src/components/accounts/profile/EditProfileModal.jsx
@@ -1,10 +1,25 @@
 import "./EditProfileModal.css";
 import { Button, Modal, Form } from "react-bootstrap";
+import { useState } from "react";
+
 export default function EditProfileModal({ show, handleClose }) {
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
-    handleClose();
+
+    if (!username || username === "") {
+      setUsernameError("Username cannot be blank!");
+    } else {
+      // No errors
+      console.log("Passed validation!", {
+        username: username,
+        description: description,
+      });
+      handleClose();
+    }
   };
 
   return (
@@ -16,11 +31,27 @@ export default function EditProfileModal({ show, handleClose }) {
         <Form>
           <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" />
+            <Form.Control
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+                setUsernameError("");
+              }}
+              isInvalid={!!usernameError}
+            />
+            <Form.Control.Feedback type="invalid">
+              {usernameError}
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
             <Form.Label>Description</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Control
+              as="textarea"
+              rows={3}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
           </Form.Group>
         </Form>
       </Modal.Body>
