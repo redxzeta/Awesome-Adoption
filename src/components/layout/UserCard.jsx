6ceda4e40f53fb902238ddaa7b056833ab717ddf
Suhhee Kim <suhhee1011@gmail.com>
2021-10-31 19:12:23 -0400

    responsive and pet display page fixed

--- src/components/layout/UserCard.jsx
+++ src/components/layout/UserCard.jsx
@@ -9,18 +9,20 @@ export default function UserCard(props) {
   const { id, photos, name, description, story } = props.userData;
 
   return (
-    <Col md={6} xs={12} key={id} className="card-container">
-      <Card className="card">
-        <Card.Body className="card__body">
+    <Col md={6} xs={12} key={id} className="user-card-container">
+      <Card className="user-card">
+        <Card.Body className="user-card__body">
           <div className="profile_Section">
-            <img src={photos} alt={name} className="card__img" />
-            <span className="card__title">{nameCleaner(name)}</span>
+            <img src={photos} alt={name} className="user-card__img" />
+            <span className="user-card__title">{nameCleaner(name)}</span>
             <br />
-            <span className="card__title desc">{nameCleaner(description)}</span>
+            <span className="user-card__title desc">
+              {nameCleaner(description)}
+            </span>
           </div>
           <div className="story_Section">
             <div className="story">{storyCleaner(story)}</div>
-            <Button className="card__btn" as={Link} to={"/404/"}>
+            <Button className="user-card__btn" as={Link} to={"/404/"}>
               More Info
             </Button>
           </div>
