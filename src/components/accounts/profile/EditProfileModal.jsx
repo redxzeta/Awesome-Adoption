a7e2cf4f8356b7d30695033b709149047c7d8269
dhillonks <kunwarvir@hotmail.com>
2021-10-31 00:47:09 -0400

    Added form for username and description

--- src/components/accounts/profile/EditProfileModal.jsx
+++ src/components/accounts/profile/EditProfileModal.jsx
@@ -1,20 +1,34 @@
 import "./EditProfileModal.css";
-import { Button, Modal } from "react-bootstrap";
-
+import { Button, Modal, Form } from "react-bootstrap";
 export default function EditProfileModal({ show, handleClose }) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    handleClose();
+  };
+
   return (
     <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
-      <Modal.Header closeButton>
+      <Modal.Header>
         <Modal.Title>Edit Profile</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>Form here</p>
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="text" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Description</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleClose}>
-          Cancel
+        <Button variant="secondary">Cancel</Button>
+        <Button variant="primary" onClick={onSubmit}>
+          Save
         </Button>
-        <Button variant="primary">Save</Button>
       </Modal.Footer>
     </Modal>
   );
