00502a34bdaade351abb41a02835d09486758719
Nathan <xzetanzero@gmail.com>
2021-11-01 15:31:11 -0700

    profile to supa (#217)
    * profile page
    
    * profile page so far
    
    * some profile changes
    
    * enhanced the design of profile
    
    * added date
    
    * basic modal setup
    
    * Added form for username and description
    
    * adding validation to the form
    
    * adding warning dialog to confirm exit
    
    * fixing order of imports
    
    * users can look up their own profile
    
    * users can update their username
    
    * profile page
    
    * adding profile + reset password
    
    * some profile changes
    
    * enhanced the design of profile
    
    * added date
    
    * basic modal setup
    
    * Added form for username and description
    
    * adding validation to the form
    
    * adding warning dialog to confirm exit
    
    * fixing order of imports
    
    * supaloading profile
    
    * users can update their username
    
    Co-authored-by: sanyogchangmai <sanyogchangmai29@gmail.com>
    Co-authored-by: dhillonks <kunwarvir@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 import {
   BrowserRouter as Router,
   Switch,
@@ -26,10 +26,11 @@ import Register from "./components/accounts/Register";
 import { supabase } from "./utils/SupaBaseUtils";
 import SLogin from "./components/accounts/SLogin";
 import { Provider } from "react-supabase";
-import { AuthProvider } from "./context/SupaContext";
+import { AuthProvider, useAuth } from "./context/SupaContext";
 import ForgotPassword from "./components/accounts/ForgotPassword";
 import ResetPassword from "./components/accounts/settings/resetPassword";
 import PrivateRoute from "./utils/PrivateRoute";
+import Profile from "./components/accounts/profile/Profile";
 
 export default function App() {
   const [token, setToken] = useState("");
@@ -104,16 +105,16 @@ export default function App() {
                     path="/forgot-password"
                     component={ForgotPassword}
                   />
-                  <Route
-                    exact
-                    path="/reset-password"
-                    component={ResetPassword}
-                  />
                   <PrivateRoute
                     exact
                     path="/reset-password"
                     component={ResetPassword}
                   />
+                  <Route exact path="/profile">
+                    <SupaLoading>
+                      <Profile />
+                    </SupaLoading>
+                  </Route>
                   <Route path="/" exact>
                     {Authenticated && <Home />}
                   </Route>
@@ -131,3 +132,9 @@ export default function App() {
     </Fragment>
   );
 }
+
+const SupaLoading = ({ children }) => {
+  const { username } = useAuth();
+  if (!username) return <Spinner />;
+  return <>{children}</>;
+};
