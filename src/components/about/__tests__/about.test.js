0d33fcf703fb6c0b566697f698946e8548c23550
redxzeta <xzetanzero@gmail.com>
2021-11-09 19:25:20 -0500

    fixed tests based on feedback from Julian

--- src/components/about/__tests__/about.test.js
+++ src/components/about/__tests__/about.test.js
@@ -1,8 +1,4 @@
-import {
-  render,
-  screen,
-  waitForElementToBeRemoved,
-} from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { rest, server } from "../../../testServer";
 import React from "react";
 
@@ -11,9 +7,14 @@ import About from "../About";
 test("should fetch api and render list", async () => {
   render(<About />);
   expect(screen.getByRole("status")).toBeInTheDocument();
-  const conList = await screen.findAllByAltText(/Contributor Avatar/i);
+  expect(screen.queryAllByAltText(/Contributor Avatar/i)).toHaveLength(0);
+  await waitFor(() =>
+    expect(screen.queryByRole("status")).not.toBeInTheDocument()
+  );
+  const conList = screen.getAllByAltText(/Contributor Avatar/i);
   expect(conList.length).toBe(2);
-  conList.forEach((y) => expect(y).toBeInTheDocument());
+  expect(conList[0]).toHaveAccessibleName("abe Contributor Avatar");
+  expect(conList[1]).toHaveAccessibleName("label Contributor Avatar");
 });
 
 test("should fetch api and expect error", async () => {
@@ -28,7 +29,13 @@ test("should fetch api and expect error", async () => {
 
   render(<About />);
   expect(screen.getByRole("status")).toBeInTheDocument();
-
+  await waitFor(() =>
+    expect(screen.queryByRole("status")).not.toBeInTheDocument()
+  );
+  const errorTitle = screen.getByRole("heading", {
+    name: /Error Loading/i,
+    level: 1,
+  });
   // const errorTitle = await screen.getByTitle(/Error Loading/i);
-  // expect(errorTitle).toBeInTheDocument()
+  expect(errorTitle).toBeInTheDocument();
 });
