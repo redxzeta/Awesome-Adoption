a2dd168e1d0c60eb0962978a9a0dea3f98474c38
dhillonks <kunwarvir@hotmail.com>
2021-11-18 13:35:15 -0500

    Updating supabase client to use fake credentials to mock the requests

--- src/testServer.js
+++ src/testServer.js
@@ -28,35 +28,35 @@ const server = setupServer(
     return res(ctx.status(200), ctx.json(contributors));
   }),
   //Login
-  rest.post(
-    "https://vuxohlizzevgcsxlrggf.supabase.co/auth/v1/token",
-    (req, res, ctx) => {
-      return res(
-        ctx.status(200),
-        ctx.json({ access_token: "fake_access_token" })
-      );
-    }
-  ),
+  rest.post("https://test.supabase.co/auth/v1/token", (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({ access_token: "fake_access_token" })
+    );
+  }),
+  rest.post("https://test.supabase.co/auth/v1/magicLink", (req, res, ctx) => {
+    return res(
+      ctx.status(400),
+      ctx.json({ message: "Unable to validate email address: invalid format" })
+    );
+  }),
   //Register
-  rest.post(
-    "https://vuxohlizzevgcsxlrggf.supabase.co/auth/v1/signup",
-    (req, res, ctx) => {
-      const fake_new_account = {
-        id: "fake_acc_id",
-        aud: "authenticated",
-        role: "authenticated",
-        email: "fake_acc_id@test.com",
-        phone: "",
-        confirmation_sent_at: Date.now(),
-        app_metadata: { provider: "email", providers: ["email"] },
-        user_metadata: {},
-        identities: [],
-        created_at: Date.now(),
-        updated_at: Date.now(),
-      };
-      return res(ctx.status(200), ctx.json(fake_new_account));
-    }
-  )
+  rest.post("https://test.supabase.co/auth/v1/signup", (req, res, ctx) => {
+    const fake_new_account = {
+      id: "fake_acc_id",
+      aud: "authenticated",
+      role: "authenticated",
+      email: "fake_acc_id@test.com",
+      phone: "",
+      confirmation_sent_at: Date.now(),
+      app_metadata: { provider: "email", providers: ["email"] },
+      user_metadata: {},
+      identities: [],
+      created_at: Date.now(),
+      updated_at: Date.now(),
+    };
+    return res(ctx.status(200), ctx.json(fake_new_account));
+  })
 );
 
 beforeAll(() => server.listen({ onUnhandledRequest: "bypass" }));
@@ -65,9 +65,8 @@ afterEach(() => {
   server.resetHandlers();
 });
 
-const SUPABASE_URL = "https://vuxohlizzevgcsxlrggf.supabase.co";
-const KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMzg4NzYwNCwiZXhwIjoxOTQ5NDYzNjA0fQ.fxjuENwSq1mulGqA4OOnrbcZu-sW3EVVfHNQjOU0vOs";
+const SUPABASE_URL = "https://test.supabase.co";
+const KEY = "test";
 const supabase = createClient(SUPABASE_URL, KEY);
 
 export { server, rest, supabase };
