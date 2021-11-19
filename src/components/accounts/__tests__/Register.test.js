06536b215206c82c7282726994b1e155c7691527
redxzeta <xzetanzero@gmail.com>
2021-11-18 20:51:19 -0800

    adding Register failure test

--- src/components/accounts/__tests__/Register.test.js
+++ src/components/accounts/__tests__/Register.test.js
@@ -1,7 +1,8 @@
 import { render, screen, waitFor, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 import { Provider } from "react-supabase";
-import { supabase } from "../../../testServer";
+import { server, supabase, rest } from "../../../testServer";
 
 import Register from "../Register";
 
@@ -24,4 +25,38 @@ test("should register sucessfully", async () => {
   await waitFor(() => {
     expect(screen.getByText(/Success/i)).toBeInTheDocument();
   });
+});
+
+test("should register unsucessfully", async () => {
+  server.use(
+    rest.post("https://test.supabase.co/auth/v1/signup", (_req, res, ctx) => {
+      return res(
+        ctx.status(404),
+        ctx.json({ error: "Error", message: "Unable To Register" })
+      );
+    })
+  );
+  render(
+    <Provider value={supabase}>
+      <Register />
+    </Provider>
+  );
+
+  expect(screen.queryByText(/Unable To Register/i)).not.toBeInTheDocument();
+  const nameField = screen.getByLabelText(/email/i);
+  userEvent.type(nameField, "bad_fake_acc_id@fake.com");
+  const passwordField = screen.getByLabelText(/password/i);
+  userEvent.type(passwordField, "bad_fake_password");
+  const submitButton = screen.getByRole("button", { name: /submit/i });
+  expect(submitButton).toBeEnabled();
+
+  userEvent.click(screen.getByText(/submit/i));
+  const LoadingButton = screen.getByRole("button", { name: /Loading.../i });
+  expect(LoadingButton).toBeDisabled();
+
+  const errorMessage = await screen.findByText(/Unable To Register/i);
+  expect(errorMessage).toBeInTheDocument();
+
+  const postButton = screen.getByRole("button", { name: /submit/i });
+  expect(postButton).toBeEnabled();
 });
