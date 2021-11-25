77cfd8d130e1db7bf6057a6e9fe4ef5786734ef2
redxzeta <xzetanzero@gmail.com>
2021-11-25 11:19:21 -0800

    zipcode tests

---
+++ src/components/pets/__tests__/pet.type.test.js
@@ -0,0 +1,34 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import PetType from "../PetType";
+import userEvent from "@testing-library/user-event";
+
+describe("Test zipcode search", () => {
+  test("check init zipcode", async () => {
+    render(
+      <BrowserRouter>
+        <PetType />
+      </BrowserRouter>
+    );
+    const goZip = await screen.findByRole("button", { name: /Go/i });
+    expect(goZip).toBeInTheDocument();
+
+    expect(goZip).toBeEnabled();
+
+    const zipForm = screen.getByLabelText(/zipcode/i);
+    expect(zipForm).toHaveValue("19019");
+  });
+
+  test("no value zipcode disabled", async () => {
+    render(
+      <BrowserRouter>
+        <PetType />
+      </BrowserRouter>
+    );
+    const zipForm = screen.getByLabelText(/zipcode/i);
+    userEvent.clear(zipForm);
+    expect(zipForm).toHaveValue("");
+    const goZip = await screen.findByRole("button", { name: /Go/i });
+    expect(goZip).toBeDisabled();
+  });
+});
