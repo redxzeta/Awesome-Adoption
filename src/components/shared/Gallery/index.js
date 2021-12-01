3171333fd747adfcc12d7256026c3d2fec299b38
drew5494 <tassoone@gmail.com>
2021-11-30 19:28:47 -0500

    Fix Carousel for multiple iamges

--- src/components/shared/Gallery/index.js
+++ src/components/shared/Gallery/index.js
@@ -1,60 +1,18 @@
-import React, { useState } from "react";
-// import PropTypes from "prop-types";
 import "./gallery.css";
 import Carousel from "react-bootstrap/Carousel";
-// import { ReactComponent as LeftArrow } from "../../../images/arrow-left.svg";
-// import { ReactComponent as RightArrow } from "../../../images/arrow-right.svg";
 
-const Gallery = ({ data, titled }) => {
-  const [index] = useState(0);
-  // const [count, setCount] = useState(0);
-
-  // const loadImage = (position) => {
-  //   const length = data.length;
-  //   setCount(position);
-  //   setIndex(((position % length) + length) % length);
-  // };
-
+const Gallery = ({ data }) => {
   return (
-    <Carousel>
-      {data.map((item) => (
-        <Carousel.Item key={index} className="image">
-          <img
-            className="d-block w-100"
-            src={data[index].src}
-            alt={data[index].title}
-            z
-          />
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <div className="gallery-slider__main">
+      <Carousel interval={1000}>
+        {data.map((item) => (
+          <Carousel.Item key={item.src}>
+            <img className="d-block w-100" src={item.src} alt={item.title} />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
   );
 };
-
-// const Thumbnail = ({ data, currentIndex, onClick }) => (
-//   <div className="gallery-slider__thumbnails">
-//     {data.map((item, index) => (
-//       <div
-//         key={index}
-//         className={`item ${currentIndex === index && "active"}`}
-//         onClick={() => onClick(index)}
-//       >
-//         <img src={item.src} alt={item.title} />
-//       </div>
-//     ))}
-//   </div>
-// );
-
-// const DataType = PropTypes.shape({
-//   title: PropTypes.string,
-//   src: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-// });
-
-// Gallery.propTypes = { data: PropTypes.arrayOf(DataType).isRequired };
-// Thumbnail.propTypes = {
-//   data: PropTypes.arrayOf(DataType).isRequired,
-//   currentIndex: PropTypes.number.isRequired,
-//   onClick: PropTypes.func.isRequired,
-// };
 
 export default Gallery;
