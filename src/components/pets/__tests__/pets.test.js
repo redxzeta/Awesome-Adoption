64057c63dd8dd901154c8d08f2355b9085c32858
drew5494 <tassoone@gmail.com>
2021-11-30 21:31:07 -0500

    New snapshot

---
+++ src/components/pets/__tests__/pets.test.js
@@ -0,0 +1,73 @@
+import { cleanup, render, screen } from "@testing-library/react";
+import Pets, { AnimalType } from "../Pets";
+import renderer from "react-test-renderer";
+import { BrowserRouter } from "react-router-dom";
+afterEach(() => {
+  cleanup();
+});
+
+test("matches animal link snapshot", () => {
+  const animalLink = {
+    img: "https://via.placeholder.com/150",
+    type: "playtpus",
+    link: "pets/playtpus",
+  };
+  const tree = renderer
+    .create(
+      <BrowserRouter>
+        <AnimalType
+          type={animalLink.type}
+          img={animalLink.img}
+          link={animalLink.link}
+        />
+      </BrowserRouter>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test("Pets component should render with button", () => {
+  render(
+    <BrowserRouter>
+      <Pets />
+    </BrowserRouter>
+  );
+  const ClickHereButton = screen.getAllByRole("button", {
+    name: /Click Here/i,
+  });
+  expect(ClickHereButton[0]).toHaveAttribute("href", "/dog");
+  expect(ClickHereButton[1]).toHaveAttribute("href", "/cat");
+  expect(ClickHereButton[2]).toHaveAttribute("href", "/bird");
+  expect(ClickHereButton[3]).toHaveAttribute("href", "/horse");
+  expect(ClickHereButton[4]).toHaveAttribute("href", "/rabbit");
+});
+
+test("should render list", () => {
+  render(
+    <BrowserRouter>
+      <Pets />
+    </BrowserRouter>
+  );
+  const petTypeList = screen.getAllByRole("link");
+  expect(petTypeList.length).toBe(5);
+  expect(petTypeList[0]).toHaveAttribute("href", "/dog");
+  expect(petTypeList[1]).toHaveAttribute("href", "/cat");
+  expect(petTypeList[2]).toHaveAttribute("href", "/bird");
+  expect(petTypeList[3]).toHaveAttribute("href", "/horse");
+  expect(petTypeList[4]).toHaveAttribute("href", "/rabbit");
+});
+
+test("should render with image", () => {
+  render(
+    <BrowserRouter>
+      <Pets />
+    </BrowserRouter>
+  );
+  const petImage = screen.getAllByRole("img");
+  expect(petImage.length).toBe(5);
+  expect(petImage[0]).toHaveAttribute("alt", "dog");
+  expect(petImage[1]).toHaveAttribute("alt", "cat");
+  expect(petImage[2]).toHaveAttribute("alt", "bird");
+  expect(petImage[3]).toHaveAttribute("alt", "horse");
+  expect(petImage[4]).toHaveAttribute("alt", "rabbit");
+});
