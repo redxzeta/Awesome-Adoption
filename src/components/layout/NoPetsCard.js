f9dbc3bbfe281efc1083bb7788d3cfc776f96612
Adrian Trujillo Duron <adrian.td96@gmail.com>
2021-12-19 13:34:23 -0600

    Added NoPetsCard to display when no pets are available

---
+++ src/components/layout/NoPetsCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Card, Col } from "react-bootstrap";
+import "./NoPetsCard.css";
+import missing from "../home/dog.jpg";
+
+export default function NoPetsCard() {
+  return (
+    <Col md={4} xs={12} className="card-container">
+      <Card className="card">
+        <Card.Header className="card__header">
+          <span className="card__title">No Pets Found!</span>
+        </Card.Header>
+        <Card.Img className="card__img" src={missing} />
+        <Card.Body className="card__body">
+          <Card.Title className="card__title">
+            <span className="card__body">
+              Please broad your search parameters!
+            </span>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+}
