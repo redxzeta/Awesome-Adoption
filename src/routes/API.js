d3bb272f05943ac7a48373e5d72355db4381ae30
Adrian Trujillo Duron <adrian.td96@gmail.com>
2021-12-19 13:35:22 -0600

    Added new route to API.js

--- src/routes/API.js
+++ src/routes/API.js
@@ -2,3 +2,9 @@ export const githubURL =
   "https://api.github.com/repos/redxzeta/Awesome-Adoption/contributors?page=1&per_page=100";
 
 export const lookUpPet = "https://api.petfinder.com/v2/animals/";
+
+export const petFinderURL = (type, page, location) => {
+  return `https://api.petfinder.com/v2/animals?type=${type}&location=${location}&limit=12&page=${
+    page || 1
+  }`;
+};
