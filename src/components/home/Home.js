3c638e82db5792e973c899c06646726d419bd03e
Iswar Mondal <rick731202@gmail.com>
2022-01-17 00:58:54 +0530

    Added a hero section on the home page instead of an image of a pug  (#272)
    * added a hero section on the home page insted of an image of a pug, some small css changes are also done
    
    * Update Home.js
    
    Co-authored-by: Nathan <xzetanzero@gmail.com>

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Button, Image, Row, Container } from "react-bootstrap";
+import { Button, Row, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Dog from "./dog.jpg";
+// import Dog from "./dog.jpg";
 import "./home.css";
 import PetCard from "../layout/PetCard";
 import LoadPlaceHolder from "../shared/PlaceHolderCard";
@@ -19,13 +19,19 @@ export default function Home() {
   } = useSWR([tokenHeaders ? randomPetsList : null, tokenHeaders], fetcher);
 
   return (
-    <div className="home__container">
-      <Image src={Dog} alt="doggo" roundedCircle className="shadow" id="dog" />
-      <h2>Adopt a Buddy Today!</h2>
-      <Button as={Link} to="/pets" variant="primary">
-        Adopt
-      </Button>
-      <div className="featured__pets">
+    <>
+      <div className="home__container">
+        <Container fluid className="hero">
+          <div className="dark__overlay">
+            <h1>Get a BFF</h1>
+            <h1>For now and forever</h1>
+          </div>
+        </Container>
+        <h2>Adopt a Buddy Today!</h2>
+        <Button as={Link} to="/pets" variant="primary">
+          Adopt
+        </Button>
+        <div className="featured__pets">
         <h2>Featured Pets</h2>
         {error ? (
           <h5>Oops! An Error Occurred Getting The Pets</h5>
@@ -59,6 +65,8 @@ export default function Home() {
       >
         Refresh
       </Button>
-    </div>
+      </div>
+    </>
+
   );
 }
