cc884eef82597e9d457ecca47787e338988b998b
munnawng <45150653+munnawng@users.noreply.github.com>
2022-01-22 21:33:57 -0500

    Added random pet to Pets.js (#264)
    * Issue256, added one random pet to Pets.js; added if statement to petInfoFetcher to allow fetching array: animals
    
    * CR comments:created RandomPet func + fixed style problem
    
    Co-authored-by: Nathan <xzetanzero@gmail.com>

--- src/components/pets/Pets.js
+++ src/components/pets/Pets.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Row, Col, Image, Button, Container } from "react-bootstrap";
-
 import "./pets.css";
 import Bird from "./bird.jpg";
 import Dog from "./doggo.jpg";
 import Cat from "./cat.jpg";
 import Horse from "./horse.jpg";
 import Rabbit from "./rabbit.jpg";
+import Placeholder from "./placeholder.jpg";
 // import Random from "./random.png";
 import { Link } from "react-router-dom";
+import useSWR from "swr";
+import { usePetAuth } from "../../context/TokenContext";
+import { fetcher } from "../../utils/petInfoFetcher";
 
 const linkData = [
   {
@@ -32,12 +35,54 @@ const linkData = [
     type: "rabbit",
   },
 ];
+
+const randomPetURL = "https://api.petfinder.com/v2/animals?limit=1&sort=random";
+let randomPetFound = false;
 // const randomIndex = Math.floor(linkData.length * Math.random());
 // const type = linkData[randomIndex].type;
 // const randomPet = { img: Random, type };
 // linkData.push(randomPet);
 
 export default function Pets() {
+  let randomPetImage = "";
+  const { tokenHeaders } = usePetAuth();
+
+  const { error, data: pet } = useSWR(
+    [tokenHeaders ? randomPetURL : null, tokenHeaders],
+    fetcher,
+    {
+      revalidateOnFocus: false,
+      revalidateIfStale: false,
+      revalidateOnReconnect: false,
+    }
+  );
+
+  // assigns randomPetImage according to if a photo is available
+  if (pet && pet.photos && pet.photos[0]) {
+    if (typeof pet.photos[0] === "string") {
+      randomPetImage = pet.photos[0];
+    } else if (pet.photos[0].large) {
+      randomPetImage = String(pet.photos[0].large);
+    } else if (pet.photos[0].medium) {
+      randomPetImage = pet.photos[0].medium;
+    } else {
+      randomPetImage = Placeholder;
+    }
+  } else {
+    randomPetImage = Placeholder;
+  }
+
+  if (error || (pet && !pet.name)) {
+    if (error) {
+      return <h1>An Error Occurred</h1>;
+    }
+    return <h1>No pet data</h1>;
+  }
+
+  if (pet) {
+    randomPetFound = true;
+  }
+
   return (
     <Container className="pawhub">
       <div className="pet__container">
@@ -76,3 +121,22 @@ export const AnimalType = ({ type, img, link }) => (
     </Button>
   </Col>
 );
+
+export const RandomPet = ({ randomPetImage, pet }) =>
+  randomPetFound ? (
+    <AnimalType
+      img={randomPetImage}
+      type={pet.name}
+      link={"/animal/" + pet.id}
+      key={pet.type}
+    />
+  ) : (
+    <Spinner
+      className="spinner_loading"
+      animation="grow"
+      variant="primary"
+      role="status"
+    >
+      <span className="visually-hidden">Loading...</span>
+    </Spinner>
+  );
