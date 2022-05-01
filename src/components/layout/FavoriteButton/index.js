a096b41b5bafffdd9db1291b2d7f26f4a64e2c04
redxzeta <xzetanzero@gmail.com>
2022-05-01 14:20:53 -0700

    feat: status favorites

--- src/components/layout/FavoriteButton/index.js
+++ src/components/layout/FavoriteButton/index.js
@@ -1,8 +1,33 @@
-import { IoIosHeart, IoIosHeartEmpty } from "react-icons/io";
+import { Button, Spinner } from "react-bootstrap";
 
-const FavoriteButton = ({ status, add }) => {
-  if (!status) return <IoIosHeartEmpty onClick={add} className="text-danger" />;
-  return <IoIosHeart onClick={add} className="text-danger" />;
+const FavoriteButton = ({ status, add, remove, loading }) => {
+  if (loading)
+    return (
+      <Button variant="outline-primary" size="md" disabled>
+        <Spinner
+          as="span"
+          animation="border"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+        />
+        <span className="visually-hidden">Loading...</span>
+      </Button>
+    );
+
+  if (!status)
+    return (
+      <Button variant="outline-primary" size="md" onClick={add}>
+        <i className="bi bi-bookmark-heart" alt="Add"></i>
+        <span className="visually-hidden">Add</span>
+      </Button>
+    );
+  return (
+    <Button variant="outline-primary" size="md" onClick={remove}>
+      <i className="bi bi-bookmark-heart-fill" alt="Remove"></i>{" "}
+      <span className="visually-hidden">Remove</span>
+    </Button>
+  );
 };
 
 export default FavoriteButton;
