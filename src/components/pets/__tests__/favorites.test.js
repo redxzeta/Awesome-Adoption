bbc5132b18f02ec261f10c79487de1b24f31604a
redxzeta <xzetanzero@gmail.com>
2022-05-03 20:07:40 -0700

    feat: Favorites page

---
+++ src/components/pets/__tests__/favorites.test.js
@@ -0,0 +1,33 @@
+import { screen, waitFor } from "@testing-library/react";
+import { Provider } from "react-supabase";
+
+import { AuthProvider } from "../../../context/SupaContext";
+import PetAuthProvider from "../../../context/TokenContext";
+import { customRender } from "../../../swrconfigtest";
+import { supabase } from "../../../testServer";
+import Favorites from "../Favorites";
+
+describe("Favorites", () => {
+  it("should render user favorite pets", async () => {
+    customRender(
+      <Provider value={supabase}>
+        <PetAuthProvider>
+          <AuthProvider>
+            <Favorites />
+          </AuthProvider>
+        </PetAuthProvider>
+      </Provider>
+    );
+    expect(screen.getAllByRole("status")).toHaveLength(3);
+    await waitFor(() =>
+      expect(screen.queryByRole("status")).not.toBeInTheDocument()
+    );
+
+    expect(
+      screen.getByRole("heading", {
+        name: /Start selecting your favorites to find your future best friend!/i,
+        level: 5,
+      })
+    ).toBeInTheDocument();
+  });
+});
