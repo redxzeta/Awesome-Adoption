6d6e29a9bbebef45c9e74ec0c01c6c88b2d6916f
MuizU <muizuvais99@gmail.com>
2022-06-26 21:14:50 +0530

    Created Inputs for updating username and description

--- src/components/accounts/settings/Settings.tsx
+++ src/components/accounts/settings/Settings.tsx
@@ -1,16 +1,56 @@
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import { Navigate } from "react-router-dom";
 import { useClient } from "react-supabase";
+import useSWR from "swr";
+import { fetchSupaProfile } from "utils/supaFetcher";
+
+import { useAuth } from "../../../context/SupaContext";
 
 export default function Settings() {
-  const { auth } = useClient();
-  const session = auth.session();
-  if (!session) {
-    return <Navigate to="/" />;
-  }
+  const client = useClient();
+  const { username: profileSearch } = useAuth();
+  const settings = { revalidateOnFocus: false };
+  const { error: errorProfile, data: profile } = useSWR(
+    profileSearch ? [client, profileSearch] : null,
+    fetchSupaProfile,
+    settings
+  );
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  if (errorProfile) return <h1>ERROR</h1>;
+  if (!profile) return <h1>Loading</h1>;
+  useEffect(() => {
+    if (!username && !!profile.username) setUsername(profile.username);
+    if (!description && !!profile.description) {
+      setDescription(profile.description);
+    }
+  }, [profile]);
+  const updateName = () => {
+    alert(`updated Name: ${username}`);
+  };
+  const updateDescription = () => {
+    alert(`updated Description: ${description}`);
+  };
   return (
     <Container>
       <h1>Settings</h1>
+      <div>
+        <label>Username</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button onClick={updateName}>Update Username</button>
+      </div>
+      <div>
+        <label>Description</label>
+        <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button onClick={updateDescription}>Update Description</button>
+      </div>
     </Container>
   );
 }
