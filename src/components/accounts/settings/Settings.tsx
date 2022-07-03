68aad2f03c82897862487a59346764af4967e536
MuizU <muizuvais99@gmail.com>
2022-07-03 23:38:07 +0530

    Added toast, update, and spinner

--- src/components/accounts/settings/Settings.tsx
+++ src/components/accounts/settings/Settings.tsx
@@ -1,7 +1,17 @@
 import { FormEvent, useEffect, useState } from "react";
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Row,
+  Spinner,
+  Toast,
+} from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import { useClient } from "react-supabase";
 import useSWR from "swr";
+import { supabase } from "utils/SupaBaseUtils";
 import { fetchSupaProfile } from "utils/supaFetcher";
 
 import { useAuth } from "../../../context/SupaContext";
@@ -9,6 +19,7 @@ import "./settings.css";
 
 export default function Settings() {
   const client = useClient();
+  const navigate = useNavigate();
   const { username: profileSearch, user } = useAuth();
   const settings = { revalidateOnFocus: false };
   const { error: errorProfile, data: profile } = useSWR(
@@ -16,28 +27,40 @@ export default function Settings() {
     fetchSupaProfile,
     settings
   );
+  const toastStatus = {
+    SUCCESS: "success",
+    ERROR: "danger",
+    DEFAULT: "default",
+  };
   const [username, setUsername] = useState("");
   const [description, setDescription] = useState("");
   const [validated, setValidated] = useState(false);
-  console.log("useAuth: ", useAuth());
+  const [isDisabled, setDisabled] = useState(false);
+  const [show, setShow] = useState(false);
+  const [toastState, setToastState] = useState(toastStatus.DEFAULT);
+  const [toastMessage, setToastMessage] = useState("");
+  const [toastHeader, setToastHeader] = useState("");
   useEffect(() => {
     if (!username && !!profile?.username) setUsername(profile?.username);
     if (!description && !!profile?.description) {
       setDescription(profile?.description);
     }
   }, [profile]);
-  if (errorProfile) return <h1>ERROR</h1>;
+  if (errorProfile) {
+    navigate("/");
+  }
   if (!profile) return <h1>Loading</h1>;
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     const form = event.currentTarget;
+    event.preventDefault();
     if (form.checkValidity() === false) {
-      event.preventDefault();
       event.stopPropagation();
       return;
     }
-    updateDetails();
-    event.preventDefault();
+    setDisabled(true);
+    await updateDetails();
     setValidated(true);
+    setDisabled(false);
   };
   interface IUpdateObj {
     username?: string;
@@ -51,32 +74,47 @@ export default function Settings() {
     if (description !== profile?.description) {
       updateObj.description = description;
     }
-    if (Object.keys(updateObj).length && !!user?.id) {
-      try {
-        const { data, error } = await client.auth.update({
-          data: {
-            description,
-            username,
-          },
-        });
-        console.log("error: ", error);
-        console.log("data: ", data);
-      } catch (e) {
-        console.log(e);
+    if (Object?.keys(updateObj).length && !!user?.id) {
+      const { error } = await supabase
+        .from("profiles")
+        .update({
+          description,
+          username,
+        })
+        .match({ id: user.id });
+
+      if (!!error && !!error.message) {
+        setToastState(toastStatus.ERROR);
+        setToastHeader("Error");
+        setToastMessage(error.message);
+      } else {
+        setToastState(toastStatus.SUCCESS);
+        setToastHeader("Success");
+        setToastMessage("Profile updated successfully");
       }
+      setShow(true);
     }
   };
   return (
     <Container className={"container"}>
+      <Toast
+        onClose={() => setShow(false)}
+        show={show}
+        delay={3000}
+        bg={toastState}
+        autohide
+      >
+        <Toast.Header>{toastHeader}</Toast.Header>
+        <Toast.Body>{toastMessage}</Toast.Body>
+      </Toast>
       <h1 className={"title"}>Settings</h1>
       <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Row>
-          <Form.Group as={Col} className={"settings-update-area"}>
-            <Form.Label className={"label"}>Username</Form.Label>
+          <Form.Group as={Col}>
+            <Form.Label>Username</Form.Label>
             <Form.Control
               required
               type="text"
-              className={"text-input"}
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
@@ -85,14 +123,13 @@ export default function Settings() {
             </Form.Control.Feedback>
           </Form.Group>
         </Row>
-        <Row>
-          <Form.Group as={Col} className={"settings-update-area"}>
-            <Form.Label className={"label"}>Description</Form.Label>
+        <Row className="mt-3">
+          <Form.Group as={Col}>
+            <Form.Label>Description</Form.Label>
             <Form.Control
               required
               as="textarea"
               rows={3}
-              className={"text-input"}
               value={description}
               onChange={(e) => setDescription(e.target.value)}
             />
@@ -104,9 +141,23 @@ export default function Settings() {
         <Row>
           <Button
             variant="primary"
+            disabled={
+              !!isDisabled ||
+              (username === profile?.username &&
+                description === profile?.description)
+            }
             type="submit"
             className="justify-content-center mt-3 mb-2"
           >
+            {!!isDisabled && (
+              <Spinner
+                size="sm"
+                role="status"
+                as="span"
+                variant="light"
+                animation="border"
+              />
+            )}{" "}
             Submit
           </Button>
         </Row>
