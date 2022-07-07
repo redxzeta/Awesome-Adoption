8b568e34f671942dec616847442f252a0a4dec8c
Muiz Uvais <35157872+MuizU@users.noreply.github.com>
2022-07-07 11:03:45 +0530

    feat: Created Settings page (#345)
    * Created Settings.tsx file
    
    * Added settings to Routes as a privateRoute
    
    * Added settings to navbar
    
    * Created Inputs for updating username and description
    
    * addded classnames
    
    * Created Css file
    
    * Changed order of hooks
    
    * Moved conditional statements below useEffect
    
    * Spaced out forms
    
    * Added styling for button in form
    
    * Converted to BS Form
    
    * Added Rows to each form group and btn
    
    * Removed settings from private routes
    
    * Added toast, update, and spinner
    
    * Used bootstrap flex and removed css file
    
    * Removed unused title class

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import Register from "./components/accounts/Register";
 import SLogin from "./components/accounts/SLogin";
 import Profile from "./components/accounts/profile/Profile";
 // import ResetPassword from "./components/accounts/settings/resetPassword";
+import Settings from "./components/accounts/settings/Settings";
 import Home from "./components/home/Home";
 import Organizations from "./components/organizations/Organizations";
 import Favorites from "./components/pets/Favorites";
@@ -45,6 +46,7 @@ export default function App() {
               <Route path="register" element={<Register />} />
               <Route path="login" element={<SLogin />} />
               <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route path="settings" element={<Settings />} />
               {/* <Route
                 path="reset-password"
                 element={
---
+++ src/components/accounts/settings/Settings.tsx
@@ -0,0 +1,166 @@
+import { FormEvent, useEffect, useState } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Row,
+  Spinner,
+  Toast,
+} from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import { useClient } from "react-supabase";
+import useSWR from "swr";
+import { supabase } from "utils/SupaBaseUtils";
+import { fetchSupaProfile } from "utils/supaFetcher";
+
+import { useAuth } from "../../../context/SupaContext";
+
+export default function Settings() {
+  const client = useClient();
+  const navigate = useNavigate();
+  const { username: profileSearch, user } = useAuth();
+  const settings = { revalidateOnFocus: false };
+  const { error: errorProfile, data: profile } = useSWR(
+    profileSearch ? [client, profileSearch] : null,
+    fetchSupaProfile,
+    settings
+  );
+  const toastStatus = {
+    SUCCESS: "success",
+    ERROR: "danger",
+    DEFAULT: "default",
+  };
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [validated, setValidated] = useState(false);
+  const [isDisabled, setDisabled] = useState(false);
+  const [show, setShow] = useState(false);
+  const [toastState, setToastState] = useState(toastStatus.DEFAULT);
+  const [toastMessage, setToastMessage] = useState("");
+  const [toastHeader, setToastHeader] = useState("");
+  useEffect(() => {
+    if (!username && !!profile?.username) setUsername(profile?.username);
+    if (!description && !!profile?.description) {
+      setDescription(profile?.description);
+    }
+  }, [profile]);
+  if (errorProfile) {
+    navigate("/");
+  }
+  if (!profile) return <h1>Loading</h1>;
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+    setDisabled(true);
+    await updateDetails();
+    setValidated(true);
+    setDisabled(false);
+  };
+  interface IUpdateObj {
+    username?: string;
+    description?: string;
+  }
+  const updateDetails = async () => {
+    const updateObj: IUpdateObj = {};
+    if (username !== profile?.username) {
+      updateObj.username = username;
+    }
+    if (description !== profile?.description) {
+      updateObj.description = description;
+    }
+    if (Object?.keys(updateObj).length && !!user?.id) {
+      const { error } = await supabase
+        .from("profiles")
+        .update({
+          description,
+          username,
+        })
+        .match({ id: user.id });
+
+      if (!!error && !!error.message) {
+        setToastState(toastStatus.ERROR);
+        setToastHeader("Error");
+        setToastMessage(error.message);
+      } else {
+        setToastState(toastStatus.SUCCESS);
+        setToastHeader("Success");
+        setToastMessage("Profile updated successfully");
+      }
+      setShow(true);
+    }
+  };
+  return (
+    <Container className={"d-flex flex-column align-items-center"}>
+      <Toast
+        onClose={() => setShow(false)}
+        show={show}
+        delay={3000}
+        bg={toastState}
+        autohide
+      >
+        <Toast.Header>{toastHeader}</Toast.Header>
+        <Toast.Body>{toastMessage}</Toast.Body>
+      </Toast>
+      <h1>Settings</h1>
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Row>
+          <Form.Group as={Col}>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a username
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row className="mt-3">
+          <Form.Group as={Col}>
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              required
+              as="textarea"
+              rows={3}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter a description
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+        <Row>
+          <Button
+            variant="primary"
+            disabled={
+              !!isDisabled ||
+              (username === profile?.username &&
+                description === profile?.description)
+            }
+            type="submit"
+            className="justify-content-center mt-3 mb-2"
+          >
+            {!!isDisabled && (
+              <Spinner
+                size="sm"
+                role="status"
+                as="span"
+                variant="light"
+                animation="border"
+              />
+            )}{" "}
+            Submit
+          </Button>
+        </Row>
+      </Form>
+    </Container>
+  );
+}
