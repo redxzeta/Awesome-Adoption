deead79eac61c1cf321107782efaf14e91a9b998
brando <brancastillo32@gmail.com>
2022-07-07 19:34:12 -0700

    adding test for Remove button

--- src/components/pets/__tests__/favorites.test.tsx
+++ src/components/pets/__tests__/favorites.test.tsx
@@ -1,4 +1,5 @@
 import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-supabase";
 import { FavoritePets, ISupaState } from "reducers/supaReducer";
@@ -80,4 +81,69 @@ describe("Favorites", () => {
     const petCards = screen.getAllByRole("button", { name: /More Info/i });
     expect(petCards).toHaveLength(3);
   });
+});
+
+test("test remove button", async () => {
+  const start: Date = new Date();
+  const favoritePets: FavoritePets[] = [
+    {
+      id: 50,
+      pet: "2",
+      created_at: start,
+    },
+    {
+      id: 51,
+      pet: "3",
+      created_at: start,
+    },
+    {
+      id: 52,
+      pet: "4",
+      created_at: start,
+    },
+  ];
+  const supaInitialState = {
+    favoritePets: favoritePets,
+  } as ISupaState;
+  const initialState: PetTokenType = {
+    tokenHeaders: "yayeet",
+    loading: false,
+    errors: false,
+  };
+  customRender(
+    <BrowserRouter>
+      <Provider value={supabase}>
+        <PetAuthContext.Provider value={initialState}>
+          <AuthContext.Provider
+            value={{ ...supaInitialState, dispatch: () => undefined }}
+          >
+            <Favorites />
+          </AuthContext.Provider>
+        </PetAuthContext.Provider>
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const user = userEvent.setup();
+
+  expect(screen.getAllByRole("status")).toHaveLength(3);
+  await waitFor(() =>
+    expect(screen.queryAllByRole("status")).not.toHaveLength(3)
+  );
+  const rmvButton = screen.getAllByRole("button", { name: /Remove/i });
+  expect(rmvButton).toHaveLength(3);
+  await user.click(rmvButton[0]);
+  const loadingBtn = screen.getAllByRole("button", { name: "Loading" });
+  expect(loadingBtn).toHaveLength(3);
+  await waitFor(() =>
+    expect(
+      screen.queryAllByRole("button", { name: "Loading" })
+    ).not.toHaveLength(3)
+  );
+  expect(screen.getAllByRole("button", { name: /Remove/i })).toHaveLength(3);
+  await waitFor(() =>
+    expect(
+      screen.queryAllByRole("button", { name: /Remove/i })
+    ).not.toHaveLength(3)
+  );
 });
