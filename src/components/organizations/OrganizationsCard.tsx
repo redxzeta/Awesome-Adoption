1dc26741f676bdeb8ba89f0a7e09a02578f91106
Vanessa Cassandra <vanessa.cassandra@juliusbaer.com>
2022-10-03 11:34:47 +0700

    fix: make organization cards even

--- src/components/organizations/OrganizationsCard.tsx
+++ src/components/organizations/OrganizationsCard.tsx
@@ -3,13 +3,15 @@ import { Card } from "react-daisyui";
 import { CharityType } from "./Organizations";
 
 const DonateCard = ({ ch }: { ch: CharityType }) => (
-  <article className="lg:px-4 px-1 w-full md:1/3 lg:w-1/4 my-4 fadeInUp ">
-    <Card imageFull>
+  <article className="lg:px-4 px-1 w-full md:1/3 lg:w-1/4 my-4 fadeInUp flex">
+    <Card imageFull className="w-full">
       <Card.Image src={ch.logo} alt={ch.name} className="w-full max-h-80 " />
       <Card.Body>
         <Card.Title tag="h2">{ch.name}</Card.Title>
-        <p>Location: {ch.location}</p>
-        <p>Founded: {ch.founded}</p>
+        <p>
+          Location: {ch.location} <br />
+          Founded: {ch.founded}
+        </p>
         <Card.Actions className="justify-end">
           <a
             href={ch.website}
